cacc74d2224fb51f9f41da6d09c5bcd60cf8aa2d
scls19fr <s.celles@gmail.com>
2018-10-13 10:06:12 +0200

    PEP8

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,18 +1,15 @@
-import pytest
-from .config import PORT_DEFAULT
-
-# from .button import test_button
-from .checkbox import test_checkbox
-from .draw import test_draw
-from .gauge import test_gauge
-from .number import test_number
-from .page import test_page
-from .pbar import test_progressbar
-from .picture import test_picture
-from .qrcode import test_qrcode
-from .radio import test_radio
-from .raw_cmd import test_raw
-from .scroll import test_scroll
-from .slider import test_slider
-from .text import test_text
-from .waveform import test_waveform
+# from .button import test_button  # noqa
+from .checkbox import test_checkbox  # noqa
+from .draw import test_draw  # noqa
+from .gauge import test_gauge  # noqa
+from .number import test_number  # noqa
+from .page import test_page  # noqa
+from .pbar import test_progressbar  # noqa
+from .picture import test_picture  # noqa
+from .qrcode import test_qrcode  # noqa
+from .radio import test_radio  # noqa
+from .raw_cmd import test_raw  # noqa
+from .scroll import test_scroll  # noqa
+from .slider import test_slider  # noqa
+from .text import test_text  # noqa
+from .waveform import test_waveform  # noqa
